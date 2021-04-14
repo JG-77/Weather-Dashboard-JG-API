a0be0bf78a2dae38ab6cc2d893fbd8f213186090
Jessie Guadarrama <jesguadarrama98@gmail.com>
2021-04-13 19:53:27 -0700

    added html comments and created variables for functions. Attempting to create first function for geting city's location.

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var apiKey = '55a9555759d9fcad973a8dd20ec8edde';
+//api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} -->lon and lat api
+//https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} --> one call api
+var citySearchValue = document.getElementById('citySearch').value;
+var searchButton = document.getElementById('searchBtn');
+console.log(citySearchValue);
+
+function getLatandLon() {
+    var apiLocation = 'api.openweathermap.org/data/2.5/weather?q=' + citySearchValue + '&appid=' + apiKey;
+
+    fetch(apiLocation)
+
+    .then(function (response) {
+        if (response.ok) {
+          console.log(response);
+          response.json().then(function (data) {
+            console.log(data);
+            displayRepos(data, user);
+          });
+        } else {
+          alert('Error: ' + response.statusText);
+        }
+      })
+
+
+}
+
+
+searchButton.addEventListener('click', getLatandLon);
